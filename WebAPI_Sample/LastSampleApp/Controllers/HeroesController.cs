585f7aba53f7748e4388c9ba9e605f15337d5bb3
3479kenji-tanaka <quenji.tanaka@nifty.com>
2017-05-17 15:29:09 +0900

    WebAPIサンプル
    新規作成

---
+++ WebAPI_Sample/LastSampleApp/Controllers/HeroesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LastSampleApp.Models;
+
+namespace LastSampleApp.Controllers
+{
+    public class HeroesController : ApiController
+    {
+        public IQueryable<GAMA_BANK> Get()
+        {
+            List<GAMA_BANK> heroes;
+            using (var context = new JBADBEntities())
+            {
+                heroes = context.GAMA_BANK.ToList();
+            }
+            //List<T>をIQueryable<T>に変換する。
+            return heroes.AsQueryable();
+
+        }
+        public IQueryable<GAMA_BANK> Get(string bankcd)
+        {
+            List<GAMA_BANK> heroes;
+
+            using (var context = new JBADBEntities())
+            {
+                heroes = context.GAMA_BANK.Where(n => n.BANK_CD == bankcd).ToList();
+                if (heroes == null)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+            }
+            //List<T>をIQueryable<T>に変換する。
+            return heroes.AsQueryable();
+        }
+    }
+}
